518d47279f63a29186f62979c25f2ac71ee6171e
nadermandalawi <nadr.mndlawe2@gmail.com>
2025-02-17 17:44:46 +0200

    fix mobile quiz creation card on mobile view

--- src/components/forms/quiz-creation.tsx
+++ src/components/forms/quiz-creation.tsx
@@ -204,7 +204,7 @@ const QuizCreation = ({ topic: topicParam, level: levelParam }: Props) => {
   }
 
   return (
-    <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+    <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 mt-16 lg:mt-0">
       <Card className="w-[400px]">
         <CardHeader>
           <CardTitle className="text-2xl font-bold">Quiz Creation</CardTitle>
